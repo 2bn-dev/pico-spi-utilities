8064ea571d3cb4daf28ac5d5a8b15c04949da582
Matt Mills <mmills@2bn.net>
2022-01-19 23:53:55 -0700

    Add spi_eraser

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a set of utilities that allow you to read, write or erase SPI flash chips using a Raspberry Pi Pico (RP2040) chip.
 
-While the pins are configurable in the code, you must use specific pins that correlate to hardware SPI functionality within the RP2040.
+While the pins are configurable in the code, you must use specific pins that correlate to hardware SPI functionality within the RP2040. This was built for Winbond flash, no guarauntees it will work correctly on other stuff at the moment.
 
 
 ### Building
@@ -33,6 +33,13 @@ hexdump -C firmware.output.file.bin | less
 ```
 
 ##### spi_eraser
+spi_eraser will erase the entire flash 1 sector (4096 kb) at a time. It will wait for a serial connection and then proceed to flash.
+
+
+```
+minicom -b 115200 -D /dev/ttyACM0
+```
+You should see messages in the minicom session as it erases.
 
 
 ##### spi_writer
---
+++ spi_eraser.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "pico/stdio.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+#include "pico/stdio_usb.h"
+
+
+#define FLASH_PAGE_SIZE         256
+#define FLASH_SECTOR_SIZE       4096
+#define FLASH_PAGE_PER_KB       1024/FLASH_PAGE_SIZE
+#define FLASH_TOTAL_SIZE        16*1024*FLASH_PAGE_PER_KB*FLASH_PAGE_SIZE
+
+#define FLASH_CMD_READ          0x03
+#define FLASH_CMD_STATUS        0x05
+#define FLASH_CMD_ERASE		0x20
+
+#define FLASH_STATUS_BUSY_MASK  0x01
+
+#define SPI_RX_PIN              0
+#define SPI_TX_PIN              3
+#define SPI_SCK_PIN             2
+#define SPI_CSN_PIN             1
+
+
+
+static inline void cs_select(uint cs_pin) {
+	asm volatile("nop \n nop \n nop"); // FIXME
+	gpio_put(cs_pin, 0);
+	asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+	asm volatile("nop \n nop \n nop"); // FIXME
+	gpio_put(cs_pin, 1);
+	asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+void __not_in_flash_func(flash_erase)(spi_inst_t *spi, uint cs_pin, uint32_t addr) {
+	cs_select(cs_pin);
+	uint8_t cmdbuf[4] = {
+		FLASH_CMD_ERASE,
+		addr >> 16,
+		addr >> 8,
+		addr
+	};
+
+	spi_write_blocking(spi, cmdbuf, 4);
+	//spi_read_blocking(spi, 0, buf, len);
+	cs_deselect(cs_pin);
+}
+
+
+void __not_in_flash_func(flash_wait_done)(spi_inst_t *spi, uint cs_pin) {
+	uint8_t status;
+	do {
+		cs_select(cs_pin);
+		uint8_t buf[2] = {FLASH_CMD_STATUS, 0};
+		spi_write_read_blocking(spi, buf, buf, 2);
+		cs_deselect(cs_pin);
+		status = buf[1];
+	} while (status & FLASH_STATUS_BUSY_MASK);
+}
+
+
+int main(){
+	stdio_usb_init();
+	while(!stdio_usb_connected()){
+		sleep_ms(100);
+	}
+	sleep_ms(1000);
+	spi_init(spi0, 1*1000*1000);
+
+	gpio_set_function(SPI_RX_PIN, GPIO_FUNC_SPI);
+	gpio_set_function(SPI_SCK_PIN, GPIO_FUNC_SPI);
+	gpio_set_function(SPI_TX_PIN, GPIO_FUNC_SPI);
+
+	gpio_init(SPI_CSN_PIN);
+	gpio_put(SPI_CSN_PIN, 1);
+	gpio_set_dir(SPI_CSN_PIN, GPIO_OUT);
+
+
+	uint32_t target_addr = 0;
+
+	for(target_addr = 0; target_addr < FLASH_TOTAL_SIZE; target_addr += FLASH_SECTOR_SIZE){
+		flash_erase(spi0, SPI_CSN_PIN,  target_addr);
+		printf("Flash Erase: %d, %d\n", target_addr, FLASH_SECTOR_SIZE);
+	}
+
+	printf("\nFlash Erase [theoretically] done\n");
+	return 0;
+
+}
+
+
+
